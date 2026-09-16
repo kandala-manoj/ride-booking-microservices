e8f67821e55d705bf840af92dba85f6fa45f4a35
Manoj <manojkandala7686@gmail.com>
2026-09-16 19:55:17 +0530

    feat(booking): add payment request DTO

---
+++ bookingservice/src/main/java/com/alpha/bookingservice/dto/PaymentRequestDto.java
@@ -0,0 +1,44 @@
+package com.alpha.bookingservice.dto;
+
+public class PaymentRequestDto {
+
+    private int bookingId;
+    private int riderId;
+    private double amount;
+    private String paymentMethod;
+
+    public PaymentRequestDto() {
+    }
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public int getRiderId() {
+        return riderId;
+    }
+
+    public void setRiderId(int riderId) {
+        this.riderId = riderId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+}
